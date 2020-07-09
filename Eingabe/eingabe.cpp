476a33bbcff0d2e616861ec62270899687ce40f4
Nighfire2211 <55667630+Nighfire2211@users.noreply.github.com>
2020-07-09 13:36:47 +0200

    Add files via upload

---
+++ Eingabe/eingabe.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+ {
+	double zahl1, zahl2;
+	cout << "Taschenrechner\n\n";
+	cout << "Geben Sie die 1. Zahl ein: ";
+	cin >> zahl1;
+	cout << "Geben Sie die 2. Zahl ein: ";
+	cin >> zahl2;
+	cout << "Die Summe ergibt " << zahl1+zahl2 << endl;
+	system("PAUSE");
+		
+	return 0;
+}
